6baf7f26da18757f4f22342d8ecf78e7cc6f52c2
BorisMargarian <margarian.boris@gmail.com>
2019-04-22 15:12:17 -0400

    added username column to usertable, wrote validation tests

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,6 +3,7 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    username { "new user" }
     password { 'password' }
     password_confirmation { 'password' }
   end
