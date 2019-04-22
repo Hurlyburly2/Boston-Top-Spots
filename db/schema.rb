0ebcc58226d33637dfcd50abc560b834ec81bbb5
Heather Garcia <heather.michelle.garcia@gmail.com>
2019-04-22 15:32:42 -0400

    added model for attraction and wrote tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_153137) do
+ActiveRecord::Schema.define(version: 2019_04_22_190322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attractions", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.string "address", null: false
+    t.string "city", null: false
+    t.string "state", null: false
+    t.string "zip", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
