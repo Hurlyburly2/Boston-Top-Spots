ab51fd842ecd37a1fc3cb4160802fa7c817a30aa
Heather Garcia <heather.michelle.garcia@gmail.com>
2019-04-23 14:41:01 -0400

    started showpage

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'attractions#index'
   devise_for :users
 
-  resources :attractions, only: [:index, :new, :create]
+  resources :attractions, only: [:index, :new, :create, :show]
 end
