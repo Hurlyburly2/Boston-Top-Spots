937572acca2c392563146730705299fb84c2293b
BorisMargarian <margarian.boris@gmail.com>
2019-04-23 09:51:09 -0400

    added role column to users table, created seed data for admin users, wrote tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_182601) do
+ActiveRecord::Schema.define(version: 2019_04_23_134040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_04_22_182601) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username", null: false
+    t.string "role", default: "user", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe User, type: :model do
       expect(newUser.username).to eq('Fail Example')
       expect(newUser.email).to eq('fail@example.com')
       expect(newUser.password).to eq('password')
+      expect(newUser.role).to eq('user')
     end
   end
 
@@ -19,6 +20,17 @@ RSpec.describe User, type: :model do
       expect(newUser.errors.full_messages).to include("Username can't be blank")
       expect(newUser.errors.full_messages).to include("Email can't be blank")
       expect(newUser.errors.full_messages).to include("Password can't be blank")
+    end
+  end
+
+  feature "initializing a user with a non-default role changes it" do
+    let!(:newUser) { User.create(username: 'Fail Example', email: 'fail@example.com', password: 'password', role: "admin") }
+
+    scenario "user's role is successfully changed to admin" do
+      expect(newUser.username).to eq('Fail Example')
+      expect(newUser.email).to eq('fail@example.com')
+      expect(newUser.password).to eq('password')
+      expect(newUser.role).to eq('admin')
     end
   end
 end
