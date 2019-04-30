357e7afc82df0bc9d735962956013636215db287
BorisMargarian <margarian.boris@gmail.com>
2019-04-30 15:19:33 -0400

    added some styling to devise pages

--- app/javascript/react/components/attractionTile.js
+++ app/javascript/react/components/attractionTile.js
@@ -9,12 +9,15 @@ const AttractionTile = props => {
 
   return (
     <div>
-      <h1 id="attraction-name">{props.name}</h1>
-      <p id="attraction-description">{props.description}</p>
-      <p id="attraction-address">{props.address}</p>
-      <p id="attraction-city-state-zip">
-        {props.city}, {props.state} {props.zip}
-      </p>
+      <img src={`/${props.image}`} className="attraction-image" />
+      <div className="">
+        <h1 id="attraction-name">{props.name}</h1>
+        <p id="attraction-description">{props.description}</p>
+        <p id="attraction-address">{props.address}</p>
+        <p id="attraction-city-state-zip">
+          {props.city}, {props.state} {props.zip}
+        </p>
+      </div>
       {deleteButton}
 
     </div>
--- app/serializers/attraction_show_serializer.rb
+++ app/serializers/attraction_show_serializer.rb
@@ -1,5 +1,5 @@
 class AttractionShowSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :address, :city, :state, :zip
+  attributes :id, :name, :description, :address, :city, :state, :zip, :image_url
 
   has_many :reviews
 end
