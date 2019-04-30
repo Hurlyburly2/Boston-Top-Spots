46146bb175f0b338f833b022063c59a390f077a9
Heather Garcia <heather.michelle.garcia@gmail.com>
2019-04-30 14:15:30 -0400

    added error catching for updating review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,11 +13,18 @@ class ReviewsController < ApplicationController
   end
 
   def update
-    oldParams = Review.find(params["id"])
+    @review = Review.find(params["id"])
     params["review"]["rating"] = params["review"]["rating"].to_i
-    oldParams.update!(review_params)
-    
-    redirect_to reviews_path
+    @review.assign_attributes(review_params)
+
+    if @review.save
+      redirect_to reviews_path
+    else
+      @ratingOptions = [["1 Star", 1], ["2 Stars", 2], ["3 Stars", 3], ["4 Stars", 4], ["5 Stars", 5]]
+      flash.now[:sad] = @review.errors.full_messages.join(", ")
+      render :edit
+    end
+
   end
 
   def review_params
