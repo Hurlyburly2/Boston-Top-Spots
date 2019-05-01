037b0b2c15c3e3712d798c0c9e0db76669d81803
kemmerle <kemmerle@umich.edu>
2019-05-01 12:53:20 -0400

    Made minor changes suggested by Pat

--- app/javascript/react/components/ReviewTile.js
+++ app/javascript/react/components/ReviewTile.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 const ReviewTile = (props) => {
+  let deleteButton
   let deleteButtonId = `deleteButton${props.id}`
   if (props.currentUser !== null && props.currentUser.role === "admin") {
-     let deleteButton = <button id={deleteButtonId} onClick={props.handleDeleteReview}>Delete Review</button>
+     deleteButton = <button id={deleteButtonId} onClick={props.handleDeleteReview}>Delete Review</button>
   }
+  let upvoteButton
   let upvoteButtonId = `${props.id}`
+  let downvoteButton
   let downvoteButtonId = `${props.id}`
   if (props.currentUser !== null) {
-    let upvoteButton = <button className="upvote" id={upvoteButtonId} onClick={props.handleVote}>Upvote!</button>
-    let downvoteButton = <button className="downvote" id={downvoteButtonId} onClick={props.handleVote}>Downvote!</button>
+    upvoteButton = <button className="upvote" id={upvoteButtonId} onClick={props.handleVote}>Upvote!</button>
+    downvoteButton = <button className="downvote" id={downvoteButtonId} onClick={props.handleVote}>Downvote!</button>
   }
   return (
     <div>
